e3ad1b93d3af25c324c422a395d22732f38b1c82
clement salingue <clement.salingue@ecoles-epsi.net>
2025-12-02 16:43:08 +0100

    TP4 : personnalisation du style

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,19 @@ export default function App() {
 
   return (
     <>
+      <nav className="bg-gray-900 text-white px-6 py-4 shadow-lg">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
+          <h2 className="text-xl font-bold">CinéTech</h2>
+
+          <ul className="flex gap-6 text-sm">
+            <li className="hover:text-yellow-400 cursor-pointer">Accueil</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Films</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Top</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Contact</li>
+          </ul>
+        </div>
+      </nav>
+
       <h1 className="text-4xl font-bold text-center my-6">CinéTech</h1>
 
       <main className="max-w-7xl mx-auto mt-6">
@@ -85,7 +98,7 @@ export default function App() {
         </section>
         <section className="bg-gray-900 text-white p-6 rounded-xl shadow-xl mb-10">
           <h2 className="text-3xl font-bold mb-4 text-center">
-             Film du moment
+            Film du moment
           </h2>
 
           <div className="flex flex-col md:flex-row items-center gap-6">
@@ -99,7 +112,7 @@ export default function App() {
               <h3 className="text-2xl font-bold mb-3">{featuredMovie.title}</h3>
 
               <p className="text-yellow-400 text-xl font-semibold mb-3">
-                 Note : {featuredMovie.rating}
+                Note : {featuredMovie.rating}
               </p>
 
               <p className="text-gray-300 leading-relaxed">
@@ -109,6 +122,9 @@ export default function App() {
           </div>
         </section>
       </main>
+      <footer className="bg-gray-900 text-gray-300 text-center py-6 mt-12">
+        <p className="text-sm">© 2024 CinéTech</p>
+      </footer>
     </>
   );
 }

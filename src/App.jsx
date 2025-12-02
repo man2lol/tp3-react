5d2a66a5a5bbe657c1c80606cf30dce8cb5f8871
clement salingue <clement.salingue@ecoles-epsi.net>
2025-12-02 14:26:06 +0100

    TP3 : liste des films

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import moviesList from './data/moviesList.js';
+
 export default function App() {
-  return <h1>CinéTech</h1>;
+  console.log(moviesList);
+  return (
+    <>
+      <h1>CinéTech</h1>
+      <main>
+        <h2>Films à l'affiche</h2>
+        <ul>
+          {moviesList.map((movie) => (
+            <li key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>
+                {movie.year} — {movie.director}
+              </p>
+              <p>Note : {movie.rating}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      ),
+    </>
+  );
 }
---
+++ src/data/moviesList.js
@@ -0,0 +1,102 @@
+const moviesList = [
+  {
+    id: 1,
+    title: 'Dune : Deuxième Partie',
+    director: 'Denis Villeneuve',
+    year: 2024,
+    rating: 8.7,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/dM2eC02Dq3iMtBZZDFtXSLHfFKJ.jpg',
+    synopsis:
+      "Paul Atreides s'unit à Chani et aux Fremen tout en préparant sa vengeance contre ceux qui ont détruit sa famille.",
+  },
+  {
+    id: 2,
+    title: 'Wonder Woman 1984',
+    director: 'Patty Jenkins',
+    year: 2020,
+    rating: 5.8,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg',
+    synopsis:
+      "Diana Prince se retrouve face à un nouveau danger dans les années 80, période d'excès et de convoitise.",
+  },
+  {
+    id: 3,
+    title: 'Barbie',
+    director: 'Greta Gerwig',
+    year: 2023,
+    rating: 7.0,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg',
+    synopsis:
+      "Barbie et Ken s'aventurent dans le monde réel après avoir été expulsés de Barbieland.",
+  },
+  {
+    id: 4,
+    title: 'Spider-Man: No Way Home',
+    director: 'Jon Watts',
+    year: 2021,
+    rating: 8.2,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/jwfDFqzxBkXC5bERBZrCEfK9iii.jpg',
+    synopsis:
+      "L'identité de Spider-Man est révélée, entraînant le chaos et des menaces d'autres univers.",
+  },
+  {
+    id: 5,
+    title: 'Oppenheimer',
+    director: 'Christopher Nolan',
+    year: 2023,
+    rating: 8.4,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/boAUuJBeID7VNp4L7LNMQs8mfQS.jpg',
+    synopsis:
+      "L'histoire du physicien J. Robert Oppenheimer et de son rôle dans le développement de la bombe atomique.",
+  },
+  {
+    id: 6,
+    title: 'Avengers: Endgame',
+    director: 'Anthony Russo, Joe Russo',
+    year: 2019,
+    rating: 8.2,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/wF7jv3x51hXgkl7t5KHePuRjXc8.jpg',
+    synopsis:
+      'Les Avengers restants tentent de défaire les actions de Thanos après le claquement de doigts dévastateur.',
+  },
+  {
+    id: 7,
+    title: 'Mission Impossible: Dead Reckoning',
+    director: 'Christopher McQuarrie',
+    year: 2023,
+    rating: 7.7,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/bdYbHxECXsN169pVrTz2TobFqXb.jpg',
+    synopsis:
+      "Ethan Hunt et son équipe doivent traquer une nouvelle arme terrifiante menaçant l'humanité.",
+  },
+  {
+    id: 8,
+    title: 'Sonic the Hedgehog 3',
+    director: 'Jeff Fowler',
+    year: 2024, // Film à venir, parfait pour simuler l'actualité
+    rating: 0.0, // Rating à 0 car il n'est pas encore sorti
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/gERwLGTa6JGN4qXjkip13eDaxy1.jpg', // Affiche de Sonic
+    synopsis:
+      "Les aventures de Sonic continuent avec l'arrivée d'un nouveau rival de taille.",
+  },
+  {
+    id: 9,
+    title: 'Fast & Furious X',
+    director: 'Louis Leterrier',
+    year: 2023,
+    rating: 7.2,
+    poster:
+      'https://image.tmdb.org/t/p/w600_and_h900_face/v1467wvoQIXlVY5MtAqt3PUtOdH.jpg',
+    synopsis:
+      "Dom Toretto et sa famille sont confrontés à l'adversaire le plus mortel qu'ils aient jamais rencontré.",
+  },
+];
+export default moviesList;
